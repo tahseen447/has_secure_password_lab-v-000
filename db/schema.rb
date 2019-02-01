beec355fdce80ae28d3ee6fe486b02a37c26dc37
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-01 01:21:19 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190201010738) do
+ActiveRecord::Schema.define(version: 20190201011839) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "password"
+    t.string "name"
+    t.string "password_digest"
   end
 
 end
