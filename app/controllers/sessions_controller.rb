c6e3bff180eec6cfffb785a53a5bfdba3c07c37b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-01 21:38:48 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     return redirect_to(controller: 'sessions', action: 'new') unless user
     session[:user_id] = user.id
     @user = user
-    redirect_to controller: 'application', action: 'home'
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
