470162aa6d950a5d16a2d514ba53c197ff22ddd9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-01 21:42:10 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:user][:name])
     user = user.try(:authenticate, params[:user][:password])
-    return redirect_to(controller: 'sessions', action: 'new') unless user
+    return require_logged_in unless user
     session[:user_id] = user.id
     @user = user
     redirect_to controller: 'application', action: 'hello'
