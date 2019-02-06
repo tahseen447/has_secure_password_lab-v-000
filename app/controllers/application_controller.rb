bd4212ce1ad60a696c1a6a4113a8cbb12f6233f3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-06 23:25:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
    before_action :current_user
-   helper_method :require_logged_in
+   helper_method :require_logged_in, :current_user
 
   def current_user
       @user = (User.find_by(id: session[:user_id]) || User.new)
